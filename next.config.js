07a75e174da2188107fc5bcd0637864216c7e1b8
mj balcueva <75126648+mjbalcueva@users.noreply.github.com>
2023-07-31 16:46:11 +0800

    perf: add external rsc packages

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: [
+      '@prisma/client',
+      'bcrypt',
+      'eslint',
+      'postcss',
+      'prisma',
+      'tailwindcss',
+      'ts-node',
+      'typescript',
+    ],
+  },
+}
 
 module.exports = nextConfig
