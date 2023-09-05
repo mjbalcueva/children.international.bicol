563fdfbe1d248e1c3ec9cd56610e135c9730831e
mj balcueva <75126648+mjbalcueva@users.noreply.github.com>
2023-09-05 22:56:45 +0800

    fix: modified records

--- src/app/api/(record)/children/guardians/[id]/route.ts
+++ src/app/api/(record)/children/guardians/[id]/route.ts
@@ -5,7 +5,7 @@ import { db } from '@/lib/db'
 export async function GET(req: Request, { params }: { params: { id: string } }) {
   const child = await db.child.findUnique({
     where: { id: params.id },
-    include: { family: { include: { guardians: true } } },
+    include: { familyRecord: { include: { guardians: true } } },
   })
 
   return NextResponse.json({ child })
--- src/app/api/(record)/records/[id]/route.ts
+++ src/app/api/(record)/records/[id]/route.ts
@@ -9,7 +9,7 @@ export async function GET(req: Request, { params }: { params: { id: string } }) 
       child: {
         include: {
           childInformation: true,
-          family: {
+          familyRecord: {
             include: {
               guardians: true,
               children: {
